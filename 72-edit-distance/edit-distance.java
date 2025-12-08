0047f4e1c26b5f0adbdcf20f6d3ed54f1859bc81
DanduNandini <165766420+DanduNandini@users.noreply.github.com>
2025-12-08 14:16:46 +0530

    Time: 5 ms (60.65%) | Memory: 47.1 MB (26.42%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -9,24 +9,16 @@ class Solution {
         for (int j = 0; j <= nc; j++) {
             dp[0][j] = j;
         }
-
-        for (int i = 1; i <= nr; i++) {
+         for (int i = 1; i <= nr; i++) {
             for (int j = 1; j <= nc; j++) {
-
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = 1 + Math.min(
-                        dp[i - 1][j],            
-                        Math.min(
-                            dp[i][j - 1],      
-                            dp[i - 1][j - 1]     
-                        )
-                    );
+                    dp[i][j] = 1 + Math.min( dp[i - 1][j],            
+                                      Math.min( dp[i][j - 1], dp[i - 1][j - 1] ));
                 }
             }
         }
-
         return dp[nr][nc];
     }
 }
