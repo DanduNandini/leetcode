2fd3825b355c015f6d209ac93efd6d4964c3f42b
DanduNandini <165766420+DanduNandini@users.noreply.github.com>
2026-05-17 21:19:06 +0530

    Time: 4 ms (100.00%) | Memory: 46.2 MB (17.45%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,17 +1,22 @@
 class Solution {
     public boolean isPalindrome(int x) {
-      if (x < 0 || (x % 10 == 0 && x != 0)) {
+        if(x<0){
             return false;
         }
-
-        int reversed = 0;
-        while (x > reversed) {
-            int digit = x % 10;
-            reversed = reversed * 10 + digit;
-            x = x / 10;
+            int original=x;
+        int rev=0;
+        while(x!=0){
+        int digit =x%10;
+        rev=(rev*10)+digit;
+        x=x/10;
         }
-
-
-        return x == reversed || x == reversed / 10;
-    }  
+           return original==rev;
     }
+
+    
+    public static void main(String[] args){
+        Solution sol=new Solution();
+        int x=121;
+        System.out.println(sol.isPalindrome(x));
+    }
+}
