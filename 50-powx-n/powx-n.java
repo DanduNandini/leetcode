0854507ea859781bf6d69fa77c0a770d3e2c8b2c
DanduNandini <165766420+DanduNandini@users.noreply.github.com>
2026-05-19 20:31:22 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (21.65%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,19 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n<0){
-            n=-n;
-            x=1/x;
-        }
         double res=1;
-       while(n!=0){
-        if(n%2!=0)
-    
+        long pow=n;
+        if(pow<0){
+            x=1/x;
+            pow=-pow;
+        }
+        while(pow>0){
+         if(pow%2==1){
             res=res*x;
-
+        }
         x=x*x;
-        n=n/2;
-          
+        pow=pow/2;
     }
-    return res; 
-}
-}
+        return res;
+    }
+    }
